0e10a628f3d8386a6975bef1098bef86d148b3e3
Joera <joera@joeramulders.com>
2026-09-17 10:44:26 +0000

    styulin

--- src/pages/regelingen/config.ts
+++ src/pages/regelingen/config.ts
@@ -9,7 +9,8 @@ const pageConfig: IPageConfig = {
     cumulative: false,
     vanaf: "2025-01-01"
   },
-  filters: ["vanaf"],
+  default_filters: ["vanaf"],
+  filters: [],
   endpoints: [
     "regelingen?aggregatie=eq.maand&domein_code=eq.Totaal&regeling_code=eq.Totaal",
     "regelingen?aggregatie=eq.week&domein_code=eq.Totaal&regeling_code=eq.Totaal&order=periode.desc&periode_vanaf=gte.{VANAF}",
